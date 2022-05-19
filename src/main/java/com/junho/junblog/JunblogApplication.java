30dd4cea8c89433998be82148e95af6f2b7673a5
sseq007 <sseq007@naver.com>
2022-05-19 22:28:00 +0900

    환경세팅 완료

---
+++ src/main/java/com/junho/junblog/JunblogApplication.java
@@ -0,0 +1,13 @@
+package com.junho.junblog;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JunblogApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(JunblogApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/junho/junblog/JunblogApplicationTests.java
@@ -0,0 +1,13 @@
+package com.junho.junblog;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JunblogApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
