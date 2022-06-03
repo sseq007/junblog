ab1dc3549c8bcef899e6d3c8ede553934fd393bf
sseq007 <sseq007@naver.com>
2022-06-03 19:39:02 +0900

    글상세 보기 구현 완료

--- src/main/java/com/junho/junblog/controller/BoardController.java
+++ src/main/java/com/junho/junblog/controller/BoardController.java
@@ -8,6 +8,7 @@ import org.springframework.security.core.annotation.AuthenticationPrincipal;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 
 import com.junho.junblog.config.auth.PrincipalDetail;
 import com.junho.junblog.service.BoardService;
@@ -27,6 +28,14 @@ public class BoardController {
 		
 		return "index";
 	}
+	
+	@GetMapping("/board/{id}")
+	public String findById(@PathVariable int id,Model model) {
+		model.addAttribute("board",boardService.글상세보기(id));
+		return "/board/detail";
+		
+	}
+	
 	
 	@GetMapping({"/board/saveForm"})
 	public String saveForm() {
